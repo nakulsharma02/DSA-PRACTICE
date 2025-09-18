879ee738a468ee82828f2825852e54e39563a006
nakulsharma02 <nakulsharma123@gmail.com>
2025-09-18 23:52:44 +0530

    Type Conversion or Type Casting Of Variables

---
+++ Basics/TypeCastOrCon.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a = 'a';
+    cout <<a<< endl;
+    char ch = 98;
+    cout <<ch<< endl;
+    unsigned int u = 112;/*O/P - 112*/
+    cout <<u<< endl;
+    unsigned int u1 = -112;/*O/P - 4294967184*/
+    cout <<u1<< endl;
+}
