37704d17af7e6d84f3d1838eb2dc134b17cf742c
nakulsharma02 <nakulsharma123@gmail.com>
2025-09-22 00:06:34 +0530

    Operators

---
+++ Basics/Operators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"Enter The Two Numbers:"<<endl;
+    cin>>num1>>num2;
+    cout<<"The Given two Numbers are:"<<num1<<" and "<<num2<<endl;
+    cout<<"The Addition of Two Numbers is:"<<num1+num2<<endl;
+    cout<<"The Subtraction of Two Numbers is:"<<num1-num2<<endl;
+    cout<<"The Multiplication of Two Numbers is:"<<num1*num2<<endl;
+    cout<<"The Division of Two Numbers is:"<<float(num1)/num2<<endl;
+    cout<<"The Modulus of Two Numbers is:"<<num1%num2<<endl;
+    return 0;
+}
